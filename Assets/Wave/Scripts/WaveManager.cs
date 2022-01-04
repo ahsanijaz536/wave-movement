a56c3a1385df52ca48b0794b0d48ecb873ee323e
Osama <75355266+Shady9595@users.noreply.github.com>
2022-01-04 17:43:38 +0500

    Things Cleaned

--- Assets/Wave/Scripts/WaveManager.cs
+++ Assets/Wave/Scripts/WaveManager.cs
@@ -1,95 +1,88 @@
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using System.Collections.Generic;
 using Random = UnityEngine.Random;
-using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class Waypoints
+{
+    public Transform  m_transform;
+    [HideInInspector]
+    public Vector3    m_position;
+    [HideInInspector]
+    public Quaternion m_rotation;
+
+    public Waypoints(Transform transform)
+    {
+        m_transform = transform;
+        m_position  = Vector3.zero;
+        m_rotation  = Quaternion.identity;
+    }//Constructor() end
+
+    public void SetPosRot(Vector3 pos, Quaternion rot) => m_transform.SetPositionAndRotation(pos, rot);
+
+    public void CacheValues()
+    {
+        m_position = m_transform.position;
+        m_rotation = m_transform.rotation;
+    }//SetValues() end
+
+}//class end
 
 public class WaveManager : MonoBehaviour
 {
-    public float curveAmount;
-
-    public GameObject wavePart;
-
-    [SerializeField] private int spawnCount;
+    [SerializeField] float curveAmount;
+    [Space]
+    [SerializeField] GameObject wavePart;
+    [SerializeField] int spawnCount;
+    [SerializeField] List<Waypoints> wavePartsContainer = new List<Waypoints>();
 
     private Vector3 nextWavePartPosition = Vector3.zero;
-
-    [SerializeField] private List<Transform> wavePartsContainer = new List<Transform>();
-
-    [SerializeField] private Transform waveStartPoint;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        AddBodyPart(spawnCount);
-    }
+    void Start() => AddBodyPart(spawnCount);
 
-    void OnGUI()
+    private void Update() => MovePartsInWave();
+
+    void MovePartsInWave()
     {
-        if (GUI.Button(new Rect(10, 10, 400, 200), "Add Part"))
-            AddBodyPart(1);
-        if (GUI.Button(new Rect(600, 10, 400, 200), "Restart"))
-            SceneManager.LoadScene(0);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        MovePartsInWave();
-    }
-    private void LateUpdate()
-    {
-        wavePartsContainer[0].transform.localPosition = new Vector3(waveStartPoint.localPosition.x,wavePartsContainer[0].transform.localPosition.y,wavePartsContainer[0].transform.localPosition.z);
-    }
-    
+        if(wavePartsContainer.Count > 0)
+        {
+            for (int i = 1; i < wavePartsContainer.Count; i++)
+            {                    
+                wavePartsContainer[i].SetPosRot(Vector3.Lerp(wavePartsContainer[i].m_transform.position, new Vector3(wavePartsContainer[i-1].m_position.x, wavePartsContainer[i-1].m_position.y, wavePartsContainer[i].m_transform.position.z), Time.deltaTime * curveAmount), 
+                                                Quaternion.Euler(wavePartsContainer[i-1].m_position.x, wavePartsContainer[i-1].m_position.y, wavePartsContainer[i].m_transform.rotation.z));
+                wavePartsContainer[i-1].CacheValues();       
+            }//loop end
+        }//if end
+    }//MovePartsInWave() end
+
     public void AddBodyPart(int count)
     {
         for (int i = 0; i < count; i++)
         {
-            if (wavePartsContainer.Count == 0)
+            if(i == 0)
             {
                 var wavepart = Instantiate(wavePart, this.transform);
                 wavepart.transform.localPosition = nextWavePartPosition;
                 nextWavePartPosition = wavepart.transform.localPosition;
                 nextWavePartPosition.z += .2f;
-                wavepart.AddComponent<WavePointsManager>();
-                wavePartsContainer.Add(wavepart.transform);
+                wavePartsContainer.Add(new Waypoints(wavepart.transform));
                 wavepart.GetComponent<MeshRenderer>().material.color =
                     new Color(Random.value, Random.value, Random.value);
-            }
+            }//if end
             else
             {
-                WavePointsManager wavePoint = wavePartsContainer[wavePartsContainer.Count - 1].GetComponent<WavePointsManager>();
-                wavePoint.ClearWayPointsContainer();
+                Waypoints wavePoint = wavePartsContainer[wavePartsContainer.Count - 1];
+                wavePoint.CacheValues();
                 var wavepart = Instantiate(wavePart, this.transform);
-                nextWavePartPosition = wavePoint.wayPointsContainer[0].pointPosition;
+                nextWavePartPosition = wavePoint.m_position;
                 nextWavePartPosition.z += .2f;
                 wavepart.transform.localPosition = nextWavePartPosition;
-                wavepart.AddComponent<WavePointsManager>();
-                wavePartsContainer.Add(wavepart.transform);
-                wavepart.GetComponent<WavePointsManager>().ClearWayPointsContainer();
+                wavePartsContainer.Add(new Waypoints(wavepart.transform));
+                wavePartsContainer[wavePartsContainer.Count - 1].CacheValues();
                 wavepart.GetComponent<MeshRenderer>().material.color =
                     new Color(Random.value, Random.value, Random.value);
-            }
-        }
-    }
-
-    void MovePartsInWave()
-    {
-        if (wavePartsContainer.Count > 0)
-        {
-            for (int i = 1; i < wavePartsContainer.Count; i++)
-            {
-                 WavePointsManager points = wavePartsContainer[i-1].GetComponent<WavePointsManager>();
-                                 
-                 Vector3 partPosition = points.wayPointsContainer[0].pointPosition;
-                 Quaternion partRotation = points.wayPointsContainer[0].pointRotation;
-                
-                 wavePartsContainer[i].transform.position = Vector3.Lerp(wavePartsContainer[i].transform.position, new Vector3(partPosition.x, wavePartsContainer[i].transform.position.y, wavePartsContainer[i].transform.position.z), Time.fixedDeltaTime * curveAmount);
-                 wavePartsContainer[i].transform.rotation = Quaternion.Euler(wavePartsContainer[i].transform.rotation.x, partRotation.y, wavePartsContainer[i].transform.rotation.z);
-                                
-                 points.wayPointsContainer.RemoveAt(0);
-            }
-        }
-    }
+            }//else end
+        }//loop end
+    }//AddBodyPart() end
     
-}
+}//class end
