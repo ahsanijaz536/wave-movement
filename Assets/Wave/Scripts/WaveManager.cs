57560ca6a8562d9a9d078289bedb01b21ceab5cc
Osama <75355266+Shady9595@users.noreply.github.com>
2022-01-04 19:07:21 +0500

    asd

--- Assets/Wave/Scripts/WaveManager.cs
+++ Assets/Wave/Scripts/WaveManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using Sirenix.OdinInspector;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
+[HideLabel]
 [System.Serializable]
 public class Waypoints
 {
@@ -10,6 +12,8 @@ public class Waypoints
     public Vector3    m_position;
     [HideInInspector]
     public Quaternion m_rotation;
+    [HideInInspector]
+    public float OffsetZ = 0.2f;
 
     public Waypoints(Transform transform)
     {
@@ -28,8 +32,12 @@ public class Waypoints
 
 }//class end
 
+public enum UpdateMode{Update, FixedUpdate, LateUpdate}
+
+[HideMonoScript]
 public class WaveManager : MonoBehaviour
 {
+    [SerializeField] UpdateMode updateMode;
     [SerializeField] float curveAmount;
     [Space]
     [SerializeField] GameObject wavePart;
@@ -40,23 +48,42 @@ public class WaveManager : MonoBehaviour
     
     void Start() => AddBodyPart(spawnCount);
 
-    private void Update() => MovePartsInWave();
+    private void Update()
+    {
+        if(updateMode.Equals(UpdateMode.Update))
+            MovePartsInWave(Time.deltaTime);
+    }//Update() end
 
-    void MovePartsInWave()
+    private void FixedUpdate()
+    {
+        if(updateMode.Equals(UpdateMode.FixedUpdate))
+            MovePartsInWave(Time.fixedDeltaTime);
+    }//FixedUpdate() end
+
+    private void LateUpdate()
+    {
+        if(updateMode.Equals(UpdateMode.LateUpdate))
+            MovePartsInWave(Time.deltaTime);
+    }//LateUpdate() end
+
+    void MovePartsInWave(float DeltaTime)
     {
         if(wavePartsContainer.Count > 0)
         {
             for (int i = 1; i < wavePartsContainer.Count; i++)
-            {                    
-                wavePartsContainer[i].SetPosRot(Vector3.Lerp(wavePartsContainer[i].m_transform.position, new Vector3(wavePartsContainer[i-1].m_position.x, wavePartsContainer[i-1].m_position.y, wavePartsContainer[i].m_transform.position.z), Time.deltaTime * curveAmount), 
-                                                Quaternion.Euler(wavePartsContainer[i-1].m_position.x, wavePartsContainer[i-1].m_position.y, wavePartsContainer[i].m_transform.rotation.z));
-                wavePartsContainer[i-1].CacheValues();       
+            {     
+                Vector3 TargetPos = new Vector3(wavePartsContainer[i-1].m_position.x, wavePartsContainer[i-1].m_position.y, wavePartsContainer[i-1].m_transform.position.z + wavePartsContainer[i].OffsetZ);
+
+                wavePartsContainer[i].SetPosRot(Vector3.Lerp(wavePartsContainer[i].m_transform.position, TargetPos, Time.deltaTime * curveAmount), 
+                                                Quaternion.Lerp(wavePartsContainer[i].m_transform.rotation, wavePartsContainer[i-1].m_transform.rotation, curveAmount * Time.deltaTime));
+                wavePartsContainer[i-1].CacheValues();    
             }//loop end
         }//if end
     }//MovePartsInWave() end
 
     public void AddBodyPart(int count)
     {
+        wavePartsContainer = new List<Waypoints>();
         for (int i = 0; i < count; i++)
         {
             if(i == 0)
@@ -71,10 +98,9 @@ public class WaveManager : MonoBehaviour
             }//if end
             else
             {
-                Waypoints wavePoint = wavePartsContainer[wavePartsContainer.Count - 1];
-                wavePoint.CacheValues();
+                wavePartsContainer[wavePartsContainer.Count - 1].CacheValues();
                 var wavepart = Instantiate(wavePart, this.transform);
-                nextWavePartPosition = wavePoint.m_position;
+                nextWavePartPosition = wavePartsContainer[wavePartsContainer.Count - 1].m_position;
                 nextWavePartPosition.z += .2f;
                 wavepart.transform.localPosition = nextWavePartPosition;
                 wavePartsContainer.Add(new Waypoints(wavepart.transform));
@@ -84,5 +110,19 @@ public class WaveManager : MonoBehaviour
             }//else end
         }//loop end
     }//AddBodyPart() end
+
+    [Button("Add New Part")]
+    private void AddNewPart()
+    {
+        wavePartsContainer[wavePartsContainer.Count - 1].CacheValues();
+        var wavepart = Instantiate(wavePart, this.transform);
+        nextWavePartPosition = wavePartsContainer[wavePartsContainer.Count - 1].m_position;
+        nextWavePartPosition.z += .2f;
+        wavepart.transform.localPosition = nextWavePartPosition;
+        wavePartsContainer.Add(new Waypoints(wavepart.transform));
+        wavePartsContainer[wavePartsContainer.Count - 1].CacheValues();
+        wavepart.GetComponent<MeshRenderer>().material.color =
+            new Color(Random.value, Random.value, Random.value);
+    }//AddNewPart() end
     
 }//class end
